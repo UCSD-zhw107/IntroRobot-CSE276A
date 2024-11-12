e1288c999687f9d75046c42616b68cf1214d4e9e
Zhenyu Wu <wzy2575765661@gmail.com>
2024-11-12 02:53:56 +0000

    refernce solution

---
+++ hw3_map/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+package_name = 'hw3_map'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    py_modules=[     
+        'hw3_map.mpi_control', 
+        'hw3_map.control_node',
+        'hw3_map.slam'
+    ],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='root',
+    maintainer_email='wzy2575765661@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'slam = hw3_map.slam:main',
+            'control_node = hw3_map.control_node:main'
+        ],
+    },
+)
